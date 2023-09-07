8229980c4758991ad54cb1cc5c134ddc5cd6c5f4
kaflo <kaiodavy7@gmail.com>
2023-09-07 14:34:36 -0300

    refactor: tornar os scripts mais legíveis

--- js/app.js
+++ js/app.js
@@ -5,16 +5,15 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
-    
+
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
             </ol>
 
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
     </li>
     `
